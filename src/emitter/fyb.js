e8962cea0176c66a2c3353bd8c1fa02c80c72771
josephh <josephh@rocketmail.com>
2018-11-26 09:16:40 +0000

    all operations ok

--- src/emitter/fyb.js
+++ src/emitter/fyb.js
@@ -1,66 +1,66 @@
 var seneca = require('seneca')();
 seneca.use('../entity/catches');
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'fetch',
-//   id: 1
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'fetchAll'
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'fetchBy',
-//   angler: 'jon J',
-//   species: [
-//     'pike', 'roach'
-//   ],
-//   location: {
-//     long: "51.055551",
-//     lat: "-1.769147",
-//     radius: 10
-//   }
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'fetchByAngler',
-//   angler: 'big dave microsoft'
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'fetchBySpecies',
-//   species: ['roach', 'chub']
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'add',
-//   species: 'dace',
-//   weight: '0.7lb',
-//   length: '12cm',
-//   lat: 51.055551,
-//   long: -1.769147,
-//   photoUrls: [],
-//   angler: 'Jon J'
-// }, console.log);
-//
-// seneca.act({
-//   entity: 'catches',
-//   operation: 'remove',
-//   id: '12'}
-// , console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'fetch',
+  id: 1
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'fetchAll'
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'fetchBy',
+  angler: 'jon J',
+  species: [
+    'pike', 'roach'
+  ],
+  location: {
+    long: "51.055551",
+    lat: "-1.769147",
+    radius: 10
+  }
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'fetchByAngler',
+  angler: 'big dave microsoft'
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'fetchBySpecies',
+  species: ['roach', 'chub']
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'add',
+  species: 'dace',
+  weight: '0.7lb',
+  length: '12cm',
+  lat: 51.055551,
+  long: -1.769147,
+  photoUrls: [],
+  angler: 'Jon J'
+}, console.log);
+
+seneca.act({
+  entity: 'catches',
+  operation: 'remove',
+  id: '19'}
+, console.log);
 
 seneca.act({
   entity: 'catches',
   operation: 'update',
   id: '3',
   species: 'salmon',
-  angler: 'big dave microsoft'
+  angler: 'big xavi'
 },
 console.log);
